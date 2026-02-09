aaf6f3d3101276c59f437f1e7c102e43a030cc71
Augusto Brigida Silva <augustobrigida060@gmail.com>
2026-02-09 18:32:57 -0300

    fix:melhorando mensagem de sucesso

--- exercício2.ts
+++ exercício2.ts
@@ -1,7 +1,7 @@
 type  StatusRequisicao = "sucesso"|"erro"|"carregando";
 function mostrarMensagemStatus(status:StatusRequisicao){
     if (status==="sucesso"){
-        return "Dados carregados com sucesso"
+        return "Dados carregados com sucesso!"
 
     }else if (status==="carregando"){
         return "Aguarde... carregando dados"
